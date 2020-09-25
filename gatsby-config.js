a9a76324bfa1ecccfe1d2ba8dea737466d4dfe03
takasp <takasp.2350@gmail.com>
2020-09-26 00:32:07 +0900

    Add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,18 @@ module.exports = {
         codegenConfig: { maybeValue: "T | undefined" },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
